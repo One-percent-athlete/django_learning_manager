b687315dad9651a00fe8b36000d23f27505d50f1
One-percent-athlete <one.percent.athlete@gmail.com>
2024-07-13 00:42:40 +0900

    Add course page and authentication

--- learning_manager/course/views.py
+++ learning_manager/course/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import CourseListSerializer
+from .serializers import CourseListSerializer, CourseDetailSerializer
 from .models import Course
 
 @api_view(['GET'])
 def get_courses(request):
-    courses = Course.objects.all()
+    courses = Course.objects.all().order_by('-created_at')
     serializer = CourseListSerializer(courses, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_course(request, slug):
+    course = Course.objects.get(slug=slug)
+    serializer = CourseDetailSerializer(course)
     return Response(serializer.data) 
