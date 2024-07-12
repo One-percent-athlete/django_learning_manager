c6f5b5ae6b9948f036ea4dc34137e122a6e7528b
One-percent-athlete <one.percent.athlete@gmail.com>
2024-07-13 01:12:38 +0900

    Add lessons page and fix bug of signup and login

--- learning_manager/course/models.py
+++ learning_manager/course/models.py
@@ -19,3 +19,31 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Lesson(models.Model):
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+
+    LESSON_STATUS = (
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    )
+
+    ARTICLE = 'article'
+    QUIZ = 'quiz'
+
+    LESSON_TYPE = (
+        (ARTICLE, 'Article'),
+        (QUIZ, 'Quiz')
+    )
+
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.TextField(max_length=100, blank=True, null=True)
+    long_description = models.TextField(blank=True, null=True)
+    lesson_status = models.CharField(max_length=20, choices=LESSON_STATUS, default=DRAFT)
+    lesson_type = models.CharField(max_length=20, choices=LESSON_TYPE, default=ARTICLE)
+
+    def __str__(self):
+        return self.title
